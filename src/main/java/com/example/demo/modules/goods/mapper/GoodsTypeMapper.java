797851e318bd624f471bc91e4080d0521ec55554
muzi <900217ai>
2023-07-27 10:02:33 +0800

    提交

---
+++ src/main/java/com/example/demo/modules/goods/mapper/GoodsTypeMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.modules.goods.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.example.demo.modules.goods.entity.GoodsType;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author muzi
+ * @since 2023-07-26
+ */
+@Mapper
+public interface GoodsTypeMapper extends BaseMapper<GoodsType> {
+
+}
